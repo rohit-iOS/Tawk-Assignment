9d5290011cb7671012252df711e5edcd938da8e1
Rohit RK <rohitrkaryappa@yahoo.com>
2022-04-08 20:20:08 +0400

    Added NetworkManager for API/Service calls

---
+++ Tawk Assignment/NetworkManager.swift
@@ -0,0 +1,88 @@
+//
+//  NetworkManager.swift
+//  Tawk Assignment
+//
+//  Created by Rohit Karyappa on 08/04/2022.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+    
+    static let shared: NetworkManager = {
+        let instance = NetworkManager()
+        return instance
+    }()
+    
+    private override init() {}
+    
+    /// These are the errors this class might return
+    enum ManagerErrors: Error {
+        case invalidResponse
+        case invalidStatusCode(Int)
+    }
+
+    /// The request method you like to use
+    enum HttpMethod: String {
+        case get
+        case post
+
+        var method: String { rawValue.uppercased() }
+    }
+
+    /// Request data from an endpoint
+    /// - Parameters:
+    ///   - url: the URL
+    ///   - httpMethod: The HTTP Method to use, either get or post in this case
+    ///   - completion: The completion closure, returning a Result of either the generic type or an error
+    func request<T: Decodable>(requetparam: URLRequest?, fromURL url: URL, httpMethod: HttpMethod = .get, completion: @escaping (Result<T, Error>) -> Void) {
+
+            // Because URLSession returns on the queue it creates for the request, we need to make sure we return on one and the same queue.
+            // You can do this by either create a queue in your class (NetworkManager) which you return on, or return on the main queue.
+            let completionOnMain: (Result<T, Error>) -> Void = { result in
+                DispatchQueue.main.async {
+                    completion(result)
+                }
+            }
+
+            // Create the request. On the request you can define if it is a GET or POST request, add body and more.
+            var request = URLRequest(url: url)
+            request.httpMethod = httpMethod.method
+        
+        if let req = requetparam {
+            request = req
+        }
+
+            let urlSession = URLSession.shared.dataTask(with: request) { data, response, error in
+                
+                if let error = error {
+                    completionOnMain(.failure(error))
+                    return
+                }
+
+                
+                guard let urlResponse = response as? HTTPURLResponse else { return completionOnMain(.failure(ManagerErrors.invalidResponse)) }
+                if !(200..<300).contains(urlResponse.statusCode) {
+                    return completionOnMain(.failure(ManagerErrors.invalidStatusCode(urlResponse.statusCode)))
+                }
+
+                if let data = data as? T {
+                    completionOnMain(.success(data))
+                    return
+                }
+                
+                // Now that all our prerequisites are fullfilled, we can take our data and try to translate it to our generic type of T.
+                guard let data = data else { return }
+                do {
+                    let users = try JSONDecoder().decode(T.self, from: data)
+                    completionOnMain(.success(users))
+                } catch {
+                    debugPrint("Could not translate the data to the requested type. Reason: \(error.localizedDescription)")
+                    completionOnMain(.failure(error))
+                }
+            }
+
+            // Start the request
+            urlSession.resume()
+        }
+}
